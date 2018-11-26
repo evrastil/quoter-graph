e0f5ce78d356b05ee648948f4436555ac2415c59
Eduard <evrastil@gmail.com>
2018-11-26 23:14:35 +0100

    listCarrierServicesDeliveringIntoZone

--- index.js
+++ index.js
@@ -12,22 +12,26 @@ const g = graph.traversal().withRemote(new DriverRemoteConnection('ws://localhos
 const initGraph = async () => {
     await g.V().drop().iterate()
 
-    await g.addV("carrier").property("name", "UPS").as("UPS").
-    addV("carrier").property("name", "PostNL").as("PostNL").
-    addV("carrier").property("name", "DHL").as("DHL").
-    addV("zone").property("zip", "1234").as("Zone1234").
-    addE("delivers").from_("UPS").to("Zone1234").property("days", "3").property("rate", "5 USD").
-    addE("delivers").from_("DHL").to("Zone1234").property("days", "1").property("rate", "4 USD").
-    addE("delivers").from_("PostNL").to("Zone1234").property("days", "4").property("rate", "2 USD").iterate()
+    await g.addV("carrier").property("name", "UPS").as("UPS")
+        .addV("carrier").property("name", "PostNL").as("PostNL")
+        .addV("carrier").property("name", "DHL").as("DHL").addV("zone").property("zip", "1234").property("country", "NL").as("ZoneNL1234")
+        .addV("carrier").property("name", "DHL").as("DHL").addV("zone").property("zip", "6546").property("country", "DE").as("ZoneDE6546")
+        .addE("delivers").from_("UPS").to("ZoneNL1234").property("days", "3").property("rate", "5 USD")
+        .addE("delivers").from_("UPS").to("ZoneDE6546").property("days", "4").property("rate", "2 USD")
+        .addE("delivers").from_("DHL").to("ZoneNL1234").property("days", "1").property("rate", "4 USD")
+        .addE("delivers").from_("DHL").to("ZoneDE6546").property("days", "2").property("rate", "3 USD")
+        .addE("delivers").from_("PostNL").to("ZoneNL1234").property("days", "4").property("rate", "2 USD").iterate()
 
 }
 
 const listCarrierServicesDeliveringIntoZone = async (zip) => {
-    return await g.V().has("zone", "zip", zip).in_("delivers").values('name').toList()
+    // return await g.V().has("zone", "zip", zip).in_("delivers").values('name').toList()
+    return await g.V().has("zone", "zip", zip).inE("delivers").as("transit", "rate")
+        .outV().as("zone").select("transit", "rate", "zone").by("days").by("rate").by("name").toList()
 }
 
 const listZonesThatCarrierServiceDeliversTo = async (carrierName) => {
-    return await g.V().has("carrier", "name", carrierName).out("delivers").values('zip').toList()
+    return await g.V().has("carrier", "name", carrierName).out("delivers").valueMap().toList()
 }
 
 (async () => {
